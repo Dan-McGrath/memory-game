535394f60f563808f33338c55b88f1c6b44403ff
Dan McGrath <mcgrathd3254@yahoo.com>
2023-08-30 18:13:17 -0400

    Create gameboard

---
+++ src/components/gameboard/GameBoard.jsx
@@ -0,0 +1,21 @@
+import Card from "../card/Card.jsx"
+
+export default function GameBoard({array, scoreHandler, endRound, reset}) {
+
+    const shuffleCards = (arr) => {
+        const newArray = arr.slice(0);
+        for(let i = 0; i < newArray.length; i++) {
+            const randInt = Math.floor(Math.random() * arr.length);
+           [newArray[i], newArray[randInt]] = [newArray[randInt], newArray[i]];
+        }
+        return newArray
+    }
+
+    const arrayItems = shuffleCards(array).map((ele) => <Card reset={reset} key={ele} index={ele} scoreHandler={scoreHandler} endRound={endRound}/>)
+
+    return (
+        <>
+        {arrayItems}
+        </>
+    )
+}
